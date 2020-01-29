08ab165372e4fcc0c388dafc646760736b7c5b11
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-01-28 20:51:15 -0600

    Added a little bit of spacing for the home page

--- Public/js/home.js
+++ Public/js/home.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
       if (data) {
         for (var i = 0; i < data.length; i++) {
           $("#appendCard").prepend(
-            '<div class="card gedf-card"><div class="card-header"><div class="d-flex justify-content-between align-items-center"><div class="d-flex justify-content-between align-items-center"><div class="mr-2"><img class="rounded-circle" width="45" src="" alt="" /></div><div class="ml-2"><div class="h5 m-0">@' +
+            '<div class="m-5 card gedf-card"><div class="card-header"><div class="d-flex justify-content-between align-items-center"><div class="d-flex justify-content-between align-items-center"><div class="mr-2"><img class="rounded-circle" width="45" src="" alt="" /></div><div class="ml-2"><div class="h5 m-0">@' +
               data[i].username +
               '</div></div></div></div></div><div class="card-body"><div class="text-muted h7 mb-2"><i class="fa fa-clock-o"></i>' +
               data[i].createdAt +
