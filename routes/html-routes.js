08f70ea9832fe1a01c6bc8d78b0a469d8fa846dc
Mathew Ramirez <mathew.j.ramirez@gmail.com>
2020-01-30 00:18:56 -0600

    misspelled a path

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Public/start.html"));
+    res.sendFile(path.join(__dirname, "../Public/Start.html"));
   });
 
   // Route to the cms page
